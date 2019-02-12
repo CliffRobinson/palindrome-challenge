2918d6212eda7cf4a62e04534a265561343779ad
CliffRobinson <Clifford.F.Robinson@gmail.com>
2019-02-12 16:47:53 +1300

    first draft answer to the problem

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const itap = require('./isThisAPalindrome')
 
-const startingValue = 99
+const startingValue = 999
+const palindromePairs = [];
 
 for (var i = startingValue; i > 0; i--){
     for (var j = startingValue; j > 0; j--){
         const product = i*j
         if(itap(product)) {
-            console.log(`${i}*${j} == ${product}, which is a palindrome`)
+            //console.log(`${i}*${j} == ${product}, which is a palindrome`)
+            palindromePairs.push({
+                factorI:i,
+                factorJ:j,
+                product:product
+            })
         }
     }
 }
+//console.log(palindromePairs)
+palindromePairs.sort((a,b) => (b.product - a.product)) //sort the array from largest to smallest
+
+const bp = palindromePairs[0] //bp == biggest palindrome
+
+console.log(`The biggest palindromic number is ${bp.product}, which is ${bp.factorI}*${bp.factorJ}`)
