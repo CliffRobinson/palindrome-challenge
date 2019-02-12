09391ac495988f32f8682f2ccc91db5e4b13d940
CliffRobinson <Clifford.F.Robinson@gmail.com>
2019-02-12 15:54:34 +1300

    tests for isItAPaldindrome

--- isThisAPalindrome.test.js
+++ isThisAPalindrome.test.js
@@ -8,3 +8,27 @@ test("99: Even numbered palindromic number", ()=> {
 test("98: Even numbered non-palindromic number", ()=> {
     expect(isThisAPalindrome(98)).toBeFalsy()
 })
+
+test("101: Odd numbered palindromic number", ()=> {
+    expect(isThisAPalindrome(101)).toBeTruthy()
+})
+
+test("103: Odd numbered non-palindromic number", ()=> {
+    expect(isThisAPalindrome(103)).toBeFalsy()
+})
+
+test("racecar: palindromic odd numbered string", ()=> {
+    expect(isThisAPalindrome("racecar")).toBeTruthy()
+})
+
+test("bacecar: non-palindromic odd numbered string", ()=> {
+    expect(isThisAPalindrome("bacecar")).toBeFalsy()
+})
+
+test("neveroddoreven: even numbered palindromic string", ()=> {
+    expect(isThisAPalindrome("neveroddoreven")).toBeTruthy()
+})
+
+test("leveroddoreven: even numbered non-palindromic string", ()=> {
+    expect(isThisAPalindrome("leveroddoreven")).toBeFalsy()
+})
