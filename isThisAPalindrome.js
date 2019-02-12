b011ed70fbd02cd2d21e2b8e5ba8cafc536f3b78
CliffRobinson <Clifford.F.Robinson@gmail.com>
2019-02-12 15:20:39 +1300

    isThisAPalindrome compares correctly for odd and even numbers of input letters

--- isThisAPalindrome.js
+++ isThisAPalindrome.js
@@ -1,8 +1,18 @@
 
 function isThisAPalindrome(input) {
-    const inputArray = input.toString().split("");
-    console.log(inputArray);
+    const inputArray = input.toString().split(""); 
+    //Treat the input as a string, and store each character in an array.
+    console.log(inputArray)
     
+    
+    var last = inputArray.length -1
+
+    for (var first = 0; first < last; first++) {
+        console.log(`Comparing index ${first} to index ${last}.\n${inputArray[first]} == ${inputArray[last]}? ${inputArray[first] == inputArray[last]}`)
+        last--;
+    }
+
+
 }
 
 module.exports = isThisAPalindrome; 
--- tester.js
+++ tester.js
@@ -1,4 +1,6 @@
 const isThisAPalindrome = require('./isThisAPalindrome')
 
 isThisAPalindrome(99)
+isThisAPalindrome(101)
 isThisAPalindrome("racecar")
+isThisAPalindrome("neveroddoreven")
